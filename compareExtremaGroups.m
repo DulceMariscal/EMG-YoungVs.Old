5b46336f89a78e6bd3d4b81f6292e0f6940b4c1f
Unknown <sherlekar.samir1@gmail.com>
2018-06-04 14:02:57 -0400

    updated plotExtremaRatio function
    ratio for young fast baseline vs. young baseline ratio comparison

--- compareExtremaGroups.m
+++ compareExtremaGroups.m
@@ -1,13 +1,12 @@
 clear; close all;
 
-
 % load('extremaMatrixOld.mat')
 % load('extremaMatrixOldAllMuscles.mat')
 load('/Users/samirsherlekar/Desktop/emg/Data/extremaMatrixYoungFastBaseline.mat');
 load('/Users/samirsherlekar/Desktop/emg/Data/extremaMatrixYoung.mat')
  
-[min_M, max_M, ratio_M] = getExtremaMatrix(extremaMatrixYoungFastBaseline);
-[min_M2, max_M2, ratio_M2] = getExtremaMatrix(extremaMatrixYoung);
+[min_M2, max_M2, ratio_M2] = getExtremaMatrix(extremaMatrixYoungFastBaseline);
+[min_M, max_M, ratio_M] = getExtremaMatrix(extremaMatrixYoung);
 
 %muscleOrder={'TA', 'PER', 'SOL', 'LG', 'MG', 'BF', 'SEMB', 'SEMT', 'VM', 'VL', 'RF', 'HIP', 'ADM', 'TFL', 'GLU'};
 muscleOrder={'TA','MG','SEMT','VL','RF'};
@@ -25,20 +24,4 @@ n_muscles = length(muscleOrder);
 %plotExtremaEmgVoltages(n_muscles,muscleOrder,max_M2,10,'Max EMG Voltages For Young Subjects : Baseline',[3e-6 1e-4])
 
 
-
-
-
-% figure;
-% plot([1:5]+.2,ratioY(1:5,:),'o','MarkerEdgeColor','b');
-% hold on
-% plot([1:5]+.4,ratioY(6:10,:),'d','MarkerEdgeColor','b');
-% hold on 
-% plot([1:5]+.6,ratioO(1:5,:),'o','MarkerEdgeColor','r');
-% hold on 
-% plot([1:5]+.8,ratioO(6:10,:),'d','MarkerEdgeColor','red');
-% 
-% title('Ratio of Max to Min EMG Voltages')
-% legend('blue = young','red = old', 'circle = fast leg', 'diamond = slow leg');
-% set(gca,'XTick',1:5,'XTickLabel',{'TA','MG','SEMT','VL','RF'},'YScale','log','Ytick',(0:5:60))
-% axis([.8 6 1 100]);
-% grid on;
+plotExtremaRatioEmgVoltages(ratio_M,ratio_M2)
--- plotExtremaRatioEmgVoltages.m
+++ plotExtremaRatioEmgVoltages.m
@@ -1,18 +1,18 @@
-function plotExtremaRatioEmgVoltages( input_args )
- % figure;
-% plot([1:5]+.2,ratioY(1:5,:),'o','MarkerEdgeColor','b');
-% hold on
-% plot([1:5]+.4,ratioY(6:10,:),'d','MarkerEdgeColor','b');
-% hold on 
-% plot([1:5]+.6,ratioO(1:5,:),'o','MarkerEdgeColor','r');
-% hold on 
-% plot([1:5]+.8,ratioO(6:10,:),'d','MarkerEdgeColor','red');
-% 
-% title('Ratio of Max to Min EMG Voltages')
-% legend('blue = young','red = old', 'circle = fast leg', 'diamond = slow leg');
-% set(gca,'XTick',1:5,'XTickLabel',{'TA','MG','SEMT','VL','RF'},'YScale','log','Ytick',(0:5:60))
-% axis([.8 6 1 100]);
-% grid on;
+function plotExtremaRatioEmgVoltages(ratio1, ratio2)
+    figure;
+    plot([1:5],ratio1(1:5,:),'o','MarkerEdgeColor','b');
+    hold on
+    plot([1:5]+.2,ratio1(6:10,:),'d','MarkerEdgeColor','b');
+    hold on 
+    plot([1:5]+.4,ratio2(1:5,:),'o','MarkerEdgeColor','r');
+    hold on 
+    plot([1:5]+.6,ratio2(6:10,:),'d','MarkerEdgeColor','red');
+    
+    title('Ratio of Max to Min EMG Voltages Fast vs. Baseline')
+    legend('blue = baseline','red = fast baseline', 'circle = fast leg', 'diamond = slow leg');
+    set(gca,'XTick',1:5,'XTickLabel',{'TA','MG','SEMT','VL','RF'},'YScale','log','Ytick',(0:5:60))
+    axis([.8 6 1 60]);
+    grid on;
 
 
 end
